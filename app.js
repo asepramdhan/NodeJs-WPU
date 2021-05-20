92500fbdb4c83ba69d665a5933291b7444eddb2a
Your Name <asiepramdhan@gmail.com>
2021-05-20 12:26:56 +0700

    menuliskan string ke file (asynchronous)

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 const fs = require("fs");
 
 // menuliskan string ke file (synchronous)
-try {
-  fs.writeFileSync("data/test.txt", "Hello world secara synchronous!");
-} catch (e) {
+// try {
+//   fs.writeFileSync("data/test.txt", "Hello world secara synchronous!");
+// } catch (e) {
+//   console.log(e);
+// }
+
+// menuliskan string ke file (asynchronous)
+fs.writeFile("data/test.txt", "Hello world secara asynchronous", (e) => {
   console.log(e);
-}
+});
